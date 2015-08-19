99f7b0abd4bf5a4c5a434c54239aa4d8f9e62d45
Harry Xue <harry.xue@hotmail.com>
2015-08-19 13:28:39 +0800

    Updates to v0.1.3; staged for release

--- spoilers-arming.user.js
+++ spoilers-arming.user.js
@@ -8,125 +8,25 @@
 // @version 0.1.3
 // @grant none
 // ==/UserScript==
+/*
+	Copyright (c) 2015 Harry Xue
 
-// If spoilers are armed
-var armed = false;
+	Permission is hereby granted, free of charge, to any person obtaining a copy
+	of this software and associated documentation files (the "Software"), to deal
+	in the Software without restriction, including without limitation the rights
+	to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+	copies of the Software, and to permit persons to whom the Software is
+	furnished to do so, subject to the following conditions:
 
-// If spoilers are eligible to be armed
-var enabled = true;
+	The above copyright notice and this permission notice shall be included in all
+	copies or substantial portions of the Software.
 
-// The AGL altitude which the timer will update faster
-var targetAlt = 4000;
-
-/**
- * Checks for spoilers arming status, set on a timer
- */
-var spoilersTimer = setInterval(armSpoilers, 1500);
-function armSpoilers() {
-	var instruments = ges.aircraft.setup.instruments || undefined;
-	var hasSpoilers = instruments && instruments.spoilers !== undefined;
-	var AGL = ges.aircraft.animationValue.altitude - ges.groundElevation * metersToFeet || 0;
-	
-	if (hasSpoilers) {
-		update();
-		if (armed) {
-			if (ges.aircraft.groundContact && ges.aircraft.animationValue.airbrakesPosition === 0) {
-				controls.setters.setAirbrakes.set();
-				armed = false;
-				update();
-			}
-		}
-	} else disable();
-	
-	clearInterval(spoilersTimer);
-	if (AGL <= targetAlt) spoilersTimer = setInterval(armSpoilers, 1500);
-	else spoilersTimer = setInterval(armSpoilers, 30000);
-}
-
-/**
- * Updates the button to show armed status
- * Enables arming if eligible
- */
-function update() {
-	if (!enabled) {
-		enabled = true;
-		$('#spoilers-arming').removeClass('btn-danger').addClass('btn-default');
-	} else {
-		if (armed)
-			$('#spoilers-arming').removeClass('btn-default').addClass('btn-success');
-		else 
-			$('#spoilers-arming').removeClass('btn-success').addClass('btn-default');
-	}
-}
-
-/**
- * Disables arming
- */
-function disable() {
-	enabled = false;
-	if (!armed) $('#spoilers-arming')
-		.removeClass('btn-default')
-		.addClass('btn-danger');
-	if (armed) $('#spoilers-arming')
-		.removeClass('btn-success')
-		.addClass('btn-danger');
-	armed = false;
-}
-
-/**
- * Spoilers arming UI
- */
-var spoilersArmUI = 
-$('<div>')
-	.addClass('setup-section')
-	.css('padding-bottom','0px')
-	.append(
-		$('<div>')
-			.addClass('input-prepend input-append')
-			.css('margin-bottom','4px')
-			.append(
-				$('<span>')
-					.addClass('add-on')
-					.text('Spoilers')
-			, 	$('<button>')
-					.addClass('btn btn-default')
-					.attr('type','button')
-                    .css('height','30px')
-                   	.css('width','30px')
-					.attr('id', 'spoilers-arming')
-			)
-	).appendTo('.gefs-f-standard');
-
-/**
- * Checks for "click" on the spoilers arming button
- */
-$('#spoilers-arming').click(function() {
-	if (enabled) {
-		if (!ges.aircraft.groundContact) {
-			if (!armed) armed = true;
-			else armed = false;
-		}
-		else armed = false;
-		update();
-	}
-});
-
-/**
- * Checks for keydown "\" to set spoilers
- */
-$(document).keydown(function(event) {
-	if (event.which === 220 || event.keyCode === 220) // The "\" key
-		$('#spoilers-arming').click();
-});
-
-var oldLoad = Aircraft.prototype.load;
-Aircraft.prototype.load = function (aircraftName, coordinates, bJustReload) {
-	var oldParts = ges.aircraft.object3d._children;
-	oldLoad.call(this, aircraftName, coordinates, bJustReload);
-	var timer = setInterval(function() {
-		if (oldParts !== ges.aircraft.object3d._children) {
-			clearInterval(timer);
-			armSpoilers();
-		}
-	},16);
-};
+	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+	AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+	OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+	SOFTWARE.
+*/
+function S(){var e=ges.aircraft.setup.instruments||void 0,a=e&&void 0!==e.spoilers,s=ges.aircraft.animationValue.altitude-ges.groundElevation*metersToFeet||0;a?(T(),q&&ges.aircraft.groundContact&&0===ges.aircraft.animationValue.airbrakesPosition&&(controls.setters.setAirbrakes.set(),q=!1,T())):L(),clearInterval(x),x=gg>=s?setInterval(S,1500):setInterval(S,3e4)}function T(){p?q?$("#spoilers-arming").removeClass("btn-default").addClass("btn-success"):$("#spoilers-arming").removeClass("btn-success").addClass("btn-default"):(p=!0,$("#spoilers-arming").removeClass("btn-danger").addClass("btn-default"))}function L(){p=!1,q||$("#spoilers-arming").removeClass("btn-default").addClass("btn-danger"),q&&$("#spoilers-arming").removeClass("btn-success").addClass("btn-danger"),q=!1}var q=!1,p=!0,gg=4e3,x=setInterval(S,1500),w=$("<div>").addClass("setup-section").css("padding-bottom","0px").append($("<div>").addClass("input-prepend input-append").css("margin-bottom","4px").append($("<span>").addClass("add-on").text("Spoilers"),$("<button>").addClass("btn btn-default").attr("type","button").css("height","30px").css("width","30px").attr("id","spoilers-arming"))).appendTo(".gefs-f-standard");$("#spoilers-arming").click(function(){p&&(q=ges.aircraft.groundContact?!1:q?!1:!0,T())}),$(document).keydown(function(e){(220===e.which||220===e.keyCode)&&$("#spoilers-arming").click()});var A=Aircraft.prototype.load;Aircraft.prototype.load=function(e,a,s){var r=ges.aircraft.object3d._children;A.call(this,e,a,s);var t=setInterval(function(){r!==ges.aircraft.object3d._children&&(clearInterval(t),S())},16)};

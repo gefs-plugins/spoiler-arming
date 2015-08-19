e2c9d6be03f22223f1e8748f4f385f53727c9ad8
Harry Xue <harry.xue@hotmail.com>
2015-08-19 13:16:47 +0800

    Updates spoiler arming to v0.1.3

--- spoilers-arming.user.js
+++ spoilers-arming.user.js
@@ -5,29 +5,29 @@
 // @match http://www.gefs-online.com/gefs.php*
 // @match http://gefs-online.com/gefs.php*
 // @run-at document-end
-// @version 0.1.2
+// @version 0.1.3
 // @grant none
 // ==/UserScript==
 
+// If spoilers are armed
 var armed = false;
-var enabled = true;
-var spoilersAircrafts = { "a380": true 
-			, "md11": true
-                        , "156": true // B788
-			, "161": true // B77W
-			, "162": true // B737
-			, "164": true // B753
-			, "166": true // E190
-			, "167": true // B788
-			, "170": true // FLR04SA (aircraft name to be updated)
-			, "172": true // B742
-			, "183": true // A332
-			, "187": true // GXA03FI (aircraft name to be updated)
-};
 
-var spoilersArmTimer = setInterval(armSpoilers, 1500);
+// If spoilers are eligible to be armed
+var enabled = true;
+
+// The AGL altitude which the timer will update faster
+var targetAlt = 4000;
+
+/**
+ * Checks for spoilers arming status, set on a timer
+ */
+var spoilersTimer = setInterval(armSpoilers, 1500);
 function armSpoilers() {
-	if (spoilersAircrafts[ges.aircraft.name]) {
+	var instruments = ges.aircraft.setup.instruments || undefined;
+	var hasSpoilers = instruments && instruments.spoilers !== undefined;
+	var AGL = ges.aircraft.animationValue.altitude - ges.groundElevation * metersToFeet || 0;
+	
+	if (hasSpoilers) {
 		update();
 		if (armed) {
 			if (ges.aircraft.groundContact && ges.aircraft.animationValue.airbrakesPosition === 0) {
@@ -36,58 +36,71 @@ function armSpoilers() {
 				update();
 			}
 		}
-	}
-	else disable();
+	} else disable();
+	
+	clearInterval(spoilersTimer);
+	if (AGL <= targetAlt) spoilersTimer = setInterval(armSpoilers, 1500);
+	else spoilersTimer = setInterval(armSpoilers, 30000);
 }
 
-// Updates the button
+/**
+ * Updates the button to show armed status
+ * Enables arming if eligible
+ */
 function update() {
 	if (!enabled) {
 		enabled = true;
-		$('#spoilers-arming').removeClass('btn btn-danger').addClass('btn');
-	}
-	else {
+		$('#spoilers-arming').removeClass('btn-danger').addClass('btn-default');
+	} else {
 		if (armed)
-			$('#spoilers-arming').removeClass('btn').addClass('btn btn-success');
+			$('#spoilers-arming').removeClass('btn-default').addClass('btn-success');
 		else 
-			$('#spoilers-arming').removeClass('btn btn-success').addClass('btn');
+			$('#spoilers-arming').removeClass('btn-success').addClass('btn-default');
 	}
 }
 
-// Disables the arming spoilers function if the aircraft is not eligible
+/**
+ * Disables arming
+ */
 function disable() {
 	enabled = false;
 	if (!armed) $('#spoilers-arming')
-		.removeClass('btn')
-		.addClass('btn btn-danger');
+		.removeClass('btn-default')
+		.addClass('btn-danger');
 	if (armed) $('#spoilers-arming')
-		.removeClass('btn btn-success')
-		.addClass('btn btn-danger');
+		.removeClass('btn-success')
+		.addClass('btn-danger');
 	armed = false;
 }
 
-// The spoilers arming display/button	
+/**
+ * Spoilers arming UI
+ */
+var spoilersArmUI = 
 $('<div>')
 	.addClass('setup-section')
 	.css('padding-bottom','0px')
-	.append($('<div>')
+	.append(
+		$('<div>')
 			.addClass('input-prepend input-append')
 			.css('margin-bottom','4px')
 			.append(
 				$('<span>')
 					.addClass('add-on')
-					.text('Spoilers'),
-				$('<button>')
-					.addClass('btn')
+					.text('Spoilers')
+			, 	$('<button>')
+					.addClass('btn btn-default')
 					.attr('type','button')
-                    			.css('height','30px')
-                    			.css('width','30px')
+                    .css('height','30px')
+                   	.css('width','30px')
 					.attr('id', 'spoilers-arming')
 			)
 	).appendTo('.gefs-f-standard');
 
-// Event "click"
-$('#spoilers-arming').click (function() {
+/**
+ * Checks for "click" on the spoilers arming button
+ */
+$('#spoilers-arming').click(function() {
 	if (enabled) {
 		if (!ges.aircraft.groundContact) {
 			if (!armed) armed = true;
@@ -97,3 +110,23 @@ $('#spoilers-arming').click (function() {
 		update();
 	}
 });
+
+/**
+ * Checks for keydown "\" to set spoilers
+ */
+$(document).keydown(function(event) {
+	if (event.which === 220 || event.keyCode === 220) // The "\" key
+		$('#spoilers-arming').click();
+});
+
+var oldLoad = Aircraft.prototype.load;
+Aircraft.prototype.load = function (aircraftName, coordinates, bJustReload) {
+	var oldParts = ges.aircraft.object3d._children;
+	oldLoad.call(this, aircraftName, coordinates, bJustReload);
+	var timer = setInterval(function() {
+		if (oldParts !== ges.aircraft.object3d._children) {
+			clearInterval(timer);
+			armSpoilers();
+		}
+	},16);
+};

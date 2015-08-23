bfae71c2f6e95dda3ed3706d9ce6e479ca5221cb
Harry Xue <harry.xue@hotmail.com>
2015-08-23 15:52:15 +0800

    Update to v0.1.4; reworked game load implementation

--- spoilers-arming.user.js
+++ spoilers-arming.user.js
@@ -5,38 +5,45 @@
 // @match http://www.gefs-online.com/gefs.php*
 // @match http://gefs-online.com/gefs.php*
 // @run-at document-end
-// @version 0.1.3
+// @version 0.1.4
 // @grant none
 // ==/UserScript==
 
-// If spoilers are armed
-var armed = false;
+(function (fn) {
+	// Checks if the game completes loading
+	// and if all needed objects are created
+	var timer = setInterval(function () {
+		if (window.ges && ges.aircraft && ges.aircraft.setup && ges.aircraft.setup.instruments && ges.aircraft.object3d) {
+			clearInterval(timer);
+			fn();
+		}
+	});
+})(function () {
+	// If spoilers are armed
+	var armed = false;
 
-// If spoilers are eligible to be armed
-var enabled = true;
+	// If spoilers are eligible to be armed
+	var enabled = true;
 
-// The AGL altitude which the timer will update faster
-var targetAlt = 4000;
+	// The AGL altitude which the timer will update faster
+	var targetAlt = 4000;
 
-/**
- * Checks for spoilers arming status, set on a timer
- */
-var spoilersTimer = setInterval(armSpoilers, 1500);
-function armSpoilers() {
-	
-	// If an aircraft has completed loading
-	if (window.ges && ges.aircraft && ges.aircraft.setup && ges.aircraft.setup.instruments) {
-		
+	/**
+	 * Checks for spoilers arming status, set on a timer
+	 */
+	var spoilersTimer = setInterval(armSpoilers, 1500);
+
+	function armSpoilers() {
 		// If this aircraft is eligible for spoiler arming
-		var hasSpoilers = ges.aircraft.setup.instruments.spoilers !== undefined; 
-		
+		var hasSpoilers = ges.aircraft.setup.instruments.spoilers !== undefined;
+
 		// The current "At Ground Level" of the plane
-		var AGL = (ges.aircraft.animationValue.altitude || 0) - (ges.groundElevation || 0) * metersToFeet;
+		var AGL = ges.aircraft.animationValue.altitude - ges.groundElevation * metersToFeet;
 
 		if (hasSpoilers) {
 			update();
 			if (armed) {
-				if (ges.aircraft.groundContact && (ges.aircraft.animationValue.airbrakesPosition || 0) === 0) {
+				if (ges.aircraft.groundContact && ges.aircraft.animationValue.airbrakesPosition === 0) {
 					controls.setters.setAirbrakes.set();
 					armed = false;
 					update();
@@ -44,47 +51,92 @@ function armSpoilers() {
 			}
 			clearInterval(spoilersTimer);
 			if (AGL <= targetAlt) spoilersTimer = setInterval(armSpoilers, 1500);
-			else spoilersTimer = setInterval(armSpoilers, 30000);	
+			else spoilersTimer = setInterval(armSpoilers, 30000);
 		} else disable();
 	}
-}
 
-/**
- * Updates the button to show armed status
- * Enables arming if eligible
- */
-function update() {
-	if (!enabled) {
-		enabled = true;
-		$('#spoilers-arming')
-			.removeClass('btn-danger')
-			.addClass('btn-default');
-	} else {
-		if (armed)
+	/**
+	 * Updates the button to show armed status
+	 * Enables arming if eligible
+	 */
+	function update() {
+		if (!enabled) {
+			enabled = true;
 			$('#spoilers-arming')
-			.removeClass('btn-default')
-			.addClass('btn-success');
-		else
-			$('#spoilers-arming')
-			.removeClass('btn-success')
-			.addClass('btn-default');
+				.removeClass('btn-danger')
+				.addClass('btn-default');
+		} else {
+			if (armed)
+				$('#spoilers-arming')
+					.removeClass('btn-default')
+					.addClass('btn-success');
+			else
+				$('#spoilers-arming')
+					.removeClass('btn-success')
+					.addClass('btn-default');
+		}
 	}
-}
 
-/**
- * Disables arming
- */
-function disable() {
-	enabled = false;
-	if (!armed) $('#spoilers-arming')
-		.removeClass('btn-default')
-		.addClass('btn-danger');
-	if (armed) $('#spoilers-arming')
-		.removeClass('btn-success')
-		.addClass('btn-danger');
-	armed = false;
-	clearInterval(spoilersTimer);
-}
+	/**
+	 * Disables arming
+	 */
+	function disable() {
+		enabled = false;
+		if (!armed) $('#spoilers-arming')
+			.removeClass('btn-default')
+			.addClass('btn-danger');
+		if (armed) $('#spoilers-arming')
+			.removeClass('btn-success')
+			.addClass('btn-danger');
+		armed = false;
+		clearInterval(spoilersTimer);
+	}
+
+	/**
+	 * Checks for "click" on the spoilers arming button
+	 */
+	$('#spoilers-arming').click(function () {
+		if (enabled) {
+			if (!ges.aircraft.groundContact) {
+				if (!armed) armed = true;
+				else armed = false;
+			} else armed = false;
+
+			update();
+		}
+	});
+
+	/**
+	 * Checks for keydown "\" to set spoilers
+	 */
+	$(document).keydown(function (event) {
+		if (event.which === 220 || event.keyCode === 220) // The "\" key
+			$('#spoilers-arming').click();
+	});
+
+	/**
+	 * Redefines load function of Aircraft so that it 
+	 * checks for availability on aircraft load
+	 */
+	var oldLoad = Aircraft.prototype.load;
+	Aircraft.prototype.load = function (aircraftName, coordinates, bJustReload) {
+		// Obtains the old aircraft parts {Object} before loading
+		var oldParts = ges.aircraft.object3d._children;
+
+		// Calls the original function to load an aircraft
+		oldLoad.call(this, aircraftName, coordinates, bJustReload);
+
+		// Checks if the old parts refer to a different object compared 
+		// with the current parts. It's crucial to set on a timer because 
+		// it takes time for the models to load completely
+		var timer = setInterval(function () {
+			if (oldParts !== ges.aircraft.object3d._children) {
+				clearInterval(timer);
+				armSpoilers();
+			}
+		}, 16);
+	};
+});
 
 /**
  * Spoilers arming UI
@@ -108,59 +160,5 @@ var spoilersArmUI =
 			.css('width', '30px')
 			.attr('id', 'spoilers-arming')
 		)
-	).appendTo('.gefs-f-standard');
-
-/**
- * Checks for "click" on the spoilers arming button
- */
-$('#spoilers-arming').click(function () {
-	if (enabled) {
-		if (!ges.aircraft.groundContact) {
-			if (!armed) armed = true;
-			else armed = false;
-		} else armed = false;
-		
-		update();
-	}
-});
-
-/**
- * Checks for keydown "\" to set spoilers
- */
-$(document).keydown(function (event) {
-	if (event.which === 220 || event.keyCode === 220) // The "\" key
-		$('#spoilers-arming').click();
-});
-
-
-/**
- * Redefines load function of Aircraft so that it 
- * checks for availability on aircraft load
- */
-var prototypeTimer = setInterval(function () {
-	
-	// If an Aircraft object is created 
-	// and the Aircraft object has completed loading
-	if (ges.aircraft && ges.aircraft.object3d) {
-		clearInterval(prototypeTimer);
-		
-		// Redefines the load() function under Aircraft
-		var oldLoad = Aircraft.prototype.load;
-		Aircraft.prototype.load = function (aircraftName, coordinates, bJustReload) {
-			// Obtains the old aircraft parts {Object} before loading
-			var oldParts = ges.aircraft.object3d._children;
-			
-			// Calls the original function to load an aircraft
-			oldLoad.call(this, aircraftName, coordinates, bJustReload);
-			
-			// Checks if the old parts refer to a different object compared with the current parts
-			// It's crucial to set on a timer because it takes time for the models to load completely
-			var timer = setInterval(function () {
-				if (oldParts !== ges.aircraft.object3d._children) {
-					clearInterval(timer);
-					armSpoilers();
-				}
-			}, 16);
-		};
-	}
-}, 16);
+	)
+	.appendTo('.gefs-f-standard');
